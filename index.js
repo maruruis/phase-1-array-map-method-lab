da688cf3bb1f16409271a04721c045b80f40d10c
Ian Kimani <ianmwng@gmail.com>
2023-09-26 10:31:45 +0300

    complete assingment

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased(){
+
+  return tutorials.map(title =>{
+    const words = title.split(' ');
+    const titleCasedWords = words.map((word) =>{
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return titleCasedWords.join(' '); 
+
+ });
+    
+    
+
+  }
+
+
+  
